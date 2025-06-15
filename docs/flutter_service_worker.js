d1eff60fdbdb0dd2b4b087a27ec9d70f9cbf5c0e
bennanisohaib <bennanisohaiboran@gmail.com>
2025-06-15 16:19:09 +0100

    Initial web version rebuilt with /ergr_desktop/ base href

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fff7db30aea1d8a27e89c3e47395ea71
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f3de4dd42de6ae4dfe27579f5548c089",
+"flutter_bootstrap.js": "6857d252b61c09956d4816433e2641a3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
